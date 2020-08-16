e832a86ed9bf0d60c077137faa615d4fa68c4cd6
Muhammad Mustapha Miah <muhammad_mustapha_miah@live.co.uk>
2020-08-17 00:42:28 +0100

    video page changes

--- src/js/videos.js
+++ src/js/videos.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const videoList = document.querySelectorAll(".video-list")[0];
     const parser = new DOMParser(); // DOMParser is a built in JS functionality. //keyword "new" creates a new object!
     fetch(
+            // `${apiURL}/search?part=snippet&channelId=${makanMustyId}&maxResults=20&key=${youtubeApiKey}`
             `${apiURL}/search?part=snippet&channelId=${makanMustyId}&maxResults=20&key=${youtubeApiKey}`
         )
         .then((response) => {
+            console.log(response);
             return response.json();
         })
         .then((responseJSON) => {
@@ -18,10 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
         .then((videos) => {
             videos.forEach((currentVideos) => {
                 console.log("current videos", currentVideos);
+                console.log("published time", currentVideos.snippet.publishTime);
                 const videoLi = parser.parseFromString(
                     `<li id=${currentVideos.snippet.title}><p>${currentVideos.snippet.title}</p>
                                     <p>${currentVideos.snippet.description}</p>
-                                    <div class="img-container" ><div><a href="https://youtu.be/${currentVideos.id.videoId}" class="vid-link"><img alt="image missing" src=${currentVideos.snippet.thumbnails.default.url} />
+                                    <div class="img-container" ><div><a href="https://youtu.be/${currentVideos.id.videoId}" class="vid-link"><img alt="image missing" src=${currentVideos.snippet.thumbnails.high.url} />
                                     <a/></div></div></li>`,
                     "text/html"
                 );
@@ -29,3 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
 });
+
+// array.sort((a,b)=>a.getTime()-b.getTime());
+// var points = [40, 100, 1, 5, 25, 10];
+// points.sort(function(a, b){return a - b});
